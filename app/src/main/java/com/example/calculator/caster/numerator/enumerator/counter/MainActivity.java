ab51d7c21f36edd4f9d08c4ee9518ec91db7fe2e
Tomas.St <lapink540@gmail.com>
2021-07-22 21:03:37 +0300

    Назначение макета activity_main.xml главным

--- app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/MainActivity.java
+++ app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_layout_second);
+        setContentView(R.layout.activity_main);
     }
 }
