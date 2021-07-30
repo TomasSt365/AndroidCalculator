639c4b3d7af0e38f8502e95330b252f51caca965
Tomas.St <lapink540@gmail.com>
2021-07-30 17:24:03 +0300

    Инициализация кнопок, для обработки нажатий

--- app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/MainActivity.java
+++ app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.calculator.caster.numerator.enumerator.counter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private int result = 0;
+    private int numberRank = 1;
+    private String resultText = "";
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultTextView = findViewById(R.id.resultText);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
+        Button button_plus = findViewById(R.id.button_plus);
+        Button button_minus = findViewById(R.id.button_minus);
+        Button button_multiply = findViewById(R.id.button_multiply);
+        Button button_divide = findViewById(R.id.button_divide);
+        Button button_percent = findViewById(R.id.button_percent);
+        Button button_delete_symbol = findViewById(R.id.button_delete_symbol);
+        Button button_all_clear = findViewById(R.id.button_all_clear);
+        Button button_comma = findViewById(R.id.button_comma);
+        Button button_equals = findViewById(R.id.button_equals);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button0.setOnClickListener(this);
+
+        button_plus.setOnClickListener(this);
+        button_minus.setOnClickListener(this);
+        button_multiply.setOnClickListener(this);
+        button_divide.setOnClickListener(this);
+        button_percent.setOnClickListener(this);
+        button_delete_symbol.setOnClickListener(this);
+        button_all_clear.setOnClickListener(this);
+        button_comma.setOnClickListener(this);
+        button_equals.setOnClickListener(this);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+
+        }
+
     }
 }
