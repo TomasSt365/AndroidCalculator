f27fa38baef8ee5fbe1d98601cbc79f10c10a2c8
Tomas.St <lapink540@gmail.com>
2021-08-01 00:10:55 +0300

    Перенос инициализации View в отдельный метод

--- app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/MainActivity.java
+++ app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/MainActivity.java
@@ -22,48 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         errorTextView = findViewById(R.id.errorText);
         resultTextView.setText(data.getResultText());
 
-
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
-        Button button7 = findViewById(R.id.button7);
-        Button button8 = findViewById(R.id.button8);
-        Button button9 = findViewById(R.id.button9);
-        Button button0 = findViewById(R.id.button0);
-        Button button_plus = findViewById(R.id.button_plus);
-        Button button_minus = findViewById(R.id.button_minus);
-        Button button_multiply = findViewById(R.id.button_multiply);
-        Button button_divide = findViewById(R.id.button_divide);
-        Button button_percent = findViewById(R.id.button_percent);
-        Button button_delete_symbol = findViewById(R.id.button_delete_symbol);
-        Button button_all_clear = findViewById(R.id.button_all_clear);
-        Button button_comma = findViewById(R.id.button_comma);
-        Button button_equals = findViewById(R.id.button_equals);
-
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
-        button6.setOnClickListener(this);
-        button7.setOnClickListener(this);
-        button8.setOnClickListener(this);
-        button9.setOnClickListener(this);
-        button0.setOnClickListener(this);
-
-        button_plus.setOnClickListener(this);
-        button_minus.setOnClickListener(this);
-        button_multiply.setOnClickListener(this);
-        button_divide.setOnClickListener(this);
-        button_percent.setOnClickListener(this);
-        button_delete_symbol.setOnClickListener(this);
-        button_all_clear.setOnClickListener(this);
-        button_comma.setOnClickListener(this);
-        button_equals.setOnClickListener(this);
-
+        initView();
     }
 
     @Override
@@ -186,5 +145,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resultTextView.setText(data.getResultText());
     }
 
+    private void initView(){
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
 
+        Button button_plus = findViewById(R.id.button_plus);
+        Button button_minus = findViewById(R.id.button_minus);
+        Button button_multiply = findViewById(R.id.button_multiply);
+        Button button_divide = findViewById(R.id.button_divide);
+        Button button_percent = findViewById(R.id.button_percent);
+        Button button_delete_symbol = findViewById(R.id.button_delete_symbol);
+        Button button_all_clear = findViewById(R.id.button_all_clear);
+        Button button_comma = findViewById(R.id.button_comma);
+        Button button_equals = findViewById(R.id.button_equals);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button0.setOnClickListener(this);
+
+        button_plus.setOnClickListener(this);
+        button_minus.setOnClickListener(this);
+        button_multiply.setOnClickListener(this);
+        button_divide.setOnClickListener(this);
+        button_percent.setOnClickListener(this);
+        button_delete_symbol.setOnClickListener(this);
+        button_all_clear.setOnClickListener(this);
+        button_comma.setOnClickListener(this);
+        button_equals.setOnClickListener(this);
+    }
 }
