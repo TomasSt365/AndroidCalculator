0159213c9565e55824eb6f937c68c28e5bb92ddb
Tomas.St <lapink540@gmail.com>
2021-08-05 21:26:35 +0300

    Обработаны нажатия на RadioButtons(выбор темы)

--- app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/OptionActivity.java
+++ app/src/main/java/com/example/calculator/caster/numerator/enumerator/counter/OptionActivity.java
@@ -1,8 +1,10 @@
 package com.example.calculator.caster.numerator.enumerator.counter;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class OptionActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -10,10 +12,30 @@ public class OptionActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
+
+        initView();
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.radio_button_light_theme:
 
+                break;
+            case R.id.radio_button_night_theme:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initView(){
+        RadioButton radioButtonLightTheme = findViewById(R.id.radio_button_light_theme);
+        RadioButton radioButtonNightTheme = findViewById(R.id.radio_button_night_theme);
+
+        radioButtonLightTheme.setOnClickListener(this);
+        radioButtonNightTheme.setOnClickListener(this);
     }
 }
